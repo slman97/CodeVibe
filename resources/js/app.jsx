5404a10e5fe272b83334aea67578d308b561adc5
slman97 <slmanismael20@gmail>
2024-09-26 23:11:13 +0300

    Add Auth

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import '../css/app.css';
+import './bootstrap';
 
-function App() {
-    return <h1>Hello, React in Laravel 11.2!</h1>;
-}
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('app')).render(<App />);
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.jsx`,
+            import.meta.glob('./Pages/**/*.jsx'),
+        ),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(<App {...props} />);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    module.exports = {
-      content: [
-        './resources/**/*.blade.php',
-        './resources/**/*.jsx',
-        './resources/**/*.js',
-      ],
-      theme: {
-        extend: {},
-      },
-      plugins: [],
-    }
-    
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.jsx',
+    ],
 
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms],
+};
