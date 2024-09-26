0c583debdb3752e6a29e947306a13d48d00ec4df
slman97 <slmanismael20@gmail>
2024-09-26 22:46:39 +0300

    Add react and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    module.exports = {
+      content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.jsx',
+        './resources/**/*.js',
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    }
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
